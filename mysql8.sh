765b58c9dc11eb87e3392e5fcdf65b2b1f5f4347
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-01-20 14:53:50 +0545

    Update mysql8.sh
    v1.4

--- mysql8.sh
+++ mysql8.sh
@@ -3,7 +3,7 @@
 # Author : Sagar Malla
 # Date : 2024-Jan-20
 # Desc : This Script install the MySQl8 
-# Version : v1.3
+# Version : v1.4
 ##########################################
 echo "------------------------------------------------------------"
 echo "This Script file install the MySQL8 on your Machine."
@@ -17,15 +17,9 @@ sudo wget https://repo.mysql.com//mysql80-community-release-el9-1.noarch.rpm
 sudo yum -y localinstall mysql80-community-release-el9-1.noarch.rpm
 yum repolist enabled | grep "mysql.*-community.*"
 sudo yum -y install mysql-community-server 
-echo "------------------------------------------------------------"
-echo "MySQL service, Start, Enable and Status check."
-echo "------------------------------------------------------------"
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
 sudo systemctl status mysqld
-echo "------------------------------------------------------------"
-echo " MySQL Version check and Secure Installation."
-echo "------------------------------------------------------------"
 mysql --version  #mysql -V
 sudo grep 'temporary password' /var/log/mysqld.log
 mysql_secure_installation
