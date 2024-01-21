dfaf04b9b52a17f9e9fc9f7c3ac12fd2fd05c8a2
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-01-21 09:59:15 +0545

    Create mysql8-rhel.sh
    v1

---
+++ mysql8-rhel.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+##########################################
+# Author : Sagar Malla
+# Date : 2024-Jan-20
+# Desc : This Script install the MySQl8 
+# Version : v1
+##########################################
+echo "------------------------------------------------------------"
+echo "This Script file install the MySQL8 on your Machine."
+echo "------------------------------------------------------------"
+set -x 
+set -e
+sudo yum clean all
+sudo subscription-manager repos --enable codeready-builder-for-rhel-8-$(arch)-rpms
+sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y 
+sudo yum -y install epel-release
+dnf -y install @mysql
+sudo systemctl enable mysqld.service
+sudo systemctl start mysqld.service
+sudo systemctl status mysqld.service
+sudo mysql_secure_installation
+mysql --version  #mysql -V
+sudo grep 'temporary password' /var/log/mysqld.log
+mysql_secure_installation
+#mysql -u root -p
