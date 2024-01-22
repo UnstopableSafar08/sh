9e10b75ba6c4ad48f95fca4705a2b9a35de37f24
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-01-22 11:37:41 +0545

    Create mysql57.sh
    v1.3

---
+++ mysql57.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+##########################################
+# Author : Sagar Malla
+# Date : 2024-Jan-22
+# Desc : This Script install the MySQl_5.7
+# Version : v1.3
+##########################################
+set -x
+sudo touch /etc/yum.repos.d/mysql57-community.repo
+echo -e "[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/x86_64/
+enabled=1
+gpgcheck=0
+
+[mysql-connectors-community]
+name=MySQL Connectors Community
+baseurl=http://repo.mysql.com/yum/mysql-connectors-community/el/7/x86_64/
+enabled=1
+gpgcheck=0
+
+[mysql-tools-community]
+name=MySQL Tools Community
+baseurl=http://repo.mysql.com/yum/mysql-tools-community/el/7/x86_64/
+enabled=1
+gpgcheck=0" >> /etc/yum.repos.d/mysql56-community.repo
+
+#sudo rpm --import http://repo.mysql.com/RPM-GPG-KEY-mysql
+#sudo yum autoremove
+sudo yum install -y mysql-community-server
+
+set +x #this disable the debug mode 
+echo ''
+echo '-----------------------------------------------------------------------------------'
+echo 'This script will delete all mysql OLD-FILES and FOLDERS. Press 'y' to proceed.'
+read -n 1 -r -p "Press 'y' to delete: " response
+echo
+if [ "$response" = "y" ]; then
+    rm -rvf ./file*
+
+    echo "Files and folders deleted successfully."
+else
+    echo "Delete Operation aborted."
+fi
+echo '-----------------------------------------------------------------------------------'
+echo ''
+set -x
+sudo rm -rvf /etc/mysql && sudo rm -rvf /var/lib/mysql
+sudo systemctl start mysqld && sudo systemctl enable mysqld
+sudo grep 'temporary password' /var/log/mysqld.log
+sudo mysql_secure_installation
+#mysql -u root -p
+
