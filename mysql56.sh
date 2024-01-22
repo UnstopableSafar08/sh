4a92ca10bf37883e12f164e4432b398152a11739
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-01-22 17:18:31 +0545

    Update mysql56.sh
    v1.6

--- mysql56.sh
+++ mysql56.sh
@@ -3,7 +3,7 @@
 # Author : Sagar Malla
 # Date : 2024-Jan-22
 # Desc : This Script install the MySQl_5.6
-# Version : v1.5
+# Version : v1.6
 ##########################################
 set -x
 sudo touch /etc/yum.repos.d/mysql56-community.repo
@@ -11,22 +11,11 @@ echo -e "[mysql56-community]
 name=MySQL 5.6 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.6-community/el/7/x86_64/
 enabled=1
-gpgcheck=0
-
-[mysql-connectors-community]
-name=MySQL Connectors Community
-baseurl=http://repo.mysql.com/yum/mysql-connectors-community/el/7/x86_64/
-enabled=1
-gpgcheck=0
-
-[mysql-tools-community]
-name=MySQL Tools Community
-baseurl=http://repo.mysql.com/yum/mysql-tools-community/el/7/x86_64/
-enabled=1
 gpgcheck=0" > /etc/yum.repos.d/mysql56-community.repo
 
 #sudo rpm --import http://repo.mysql.com/RPM-GPG-KEY-mysql
 #sudo yum autoremove
+sudo rm -rvf /etc/mysql && sudo rm -rvf /var/lib/mysql
 sudo yum install -y mysql-community-server
 
 # set +x #this disable the debug mode 
@@ -46,7 +35,6 @@ sudo yum install -y mysql-community-server
 # echo ''
 # set -x
 
-#sudo rm -rvf /etc/mysql && sudo rm -rvf /var/lib/mysql
 sudo systemctl start mysqld && sudo systemctl enable mysqld
 sudo grep 'temporary password' /var/log/mysqld.log
 sudo mysql_secure_installation
