5396fb369ee5ed1073ecb76ae12a9ff85672c49c
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-01-22 14:35:05 +0545

    Update mysql56.sh
    v1.4

--- mysql56.sh
+++ mysql56.sh
@@ -3,7 +3,7 @@
 # Author : Sagar Malla
 # Date : 2024-Jan-22
 # Desc : This Script install the MySQl_5.6
-# Version : v1.3
+# Version : v1.4
 ##########################################
 set -x
 sudo touch /etc/yum.repos.d/mysql56-community.repo
@@ -23,29 +23,30 @@ gpgcheck=0
 name=MySQL Tools Community
 baseurl=http://repo.mysql.com/yum/mysql-tools-community/el/7/x86_64/
 enabled=1
-gpgcheck=0" >> /etc/yum.repos.d/mysql56-community.repo
+gpgcheck=0" > /etc/yum.repos.d/mysql56-community.repo
 
 #sudo rpm --import http://repo.mysql.com/RPM-GPG-KEY-mysql
 #sudo yum autoremove
 sudo yum install -y mysql-community-server
 
-set +x #this disable the debug mode 
-echo ''
-echo '-----------------------------------------------------------------------------------'
-echo 'This script will delete all mysql OLD-FILES and FOLDERS. Press 'y' to proceed.'
-read -n 1 -r -p "Press 'y' to delete: " response
-echo
-if [ "$response" = "y" ]; then
-    rm -rvf /etc/mysql && rm -rvf /var/lib/mysql
+# set +x #this disable the debug mode 
+# echo ''
+# echo '-----------------------------------------------------------------------------------'
+# echo 'This script will delete all mysql OLD-FILES and FOLDERS. Press 'y' to proceed.'
+# read -n 1 -r -p "Press 'y' to delete: " response
+# echo
+# if [ "$response" = "y" ]; then
+#     rm -rvf /etc/mysql && rm -rvf /var/lib/mysql
 
-    echo "Files and folders deleted successfully."
-else
-    echo "Delete Operation aborted."
-fi
-echo '-----------------------------------------------------------------------------------'
-echo ''
-set -x
+#     echo "Files and folders deleted successfully."
+# else
+#     echo "Delete Operation aborted."
+# fi
+# echo '-----------------------------------------------------------------------------------'
+# echo ''
+# set -x
 
+sudo rm -rvf /etc/mysql && sudo rm -rvf /var/lib/mysql
 sudo systemctl start mysqld && sudo systemctl enable mysqld
 sudo grep 'temporary password' /var/log/mysqld.log
 sudo mysql_secure_installation
