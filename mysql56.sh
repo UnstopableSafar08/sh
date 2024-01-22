f7aae536f306471c45a7033557a053bf70f894f7
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-01-22 14:59:09 +0545

    Update mysql56.sh
    v1.5

--- mysql56.sh
+++ mysql56.sh
@@ -3,7 +3,7 @@
 # Author : Sagar Malla
 # Date : 2024-Jan-22
 # Desc : This Script install the MySQl_5.6
-# Version : v1.4
+# Version : v1.5
 ##########################################
 set -x
 sudo touch /etc/yum.repos.d/mysql56-community.repo
@@ -46,7 +46,7 @@ sudo yum install -y mysql-community-server
 # echo ''
 # set -x
 
-sudo rm -rvf /etc/mysql && sudo rm -rvf /var/lib/mysql
+#sudo rm -rvf /etc/mysql && sudo rm -rvf /var/lib/mysql
 sudo systemctl start mysqld && sudo systemctl enable mysqld
 sudo grep 'temporary password' /var/log/mysqld.log
 sudo mysql_secure_installation
