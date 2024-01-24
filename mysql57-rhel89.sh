42a7e79903eab1b240f46fb943b36910fa61675d
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-01-24 15:38:13 +0545

    Update mysql57-rhel89.sh
    v1.3

--- mysql57-rhel89.sh
+++ mysql57-rhel89.sh
@@ -3,7 +3,7 @@
 # Author : Sagar Malla
 # Date : 2024-Jan-21
 # Desc : This Script install the MySQl5.7 on RHEL
-# Version : v1.2
+# Version : v1.3
 ##########################################
 
 set -x
@@ -16,24 +16,11 @@ echo -e "[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/x86_64/
 enabled=1
-gpgcheck=0
-
-[mysql-connectors-community]
-name=MySQL Connectors Community
-baseurl=http://repo.mysql.com/yum/mysql-connectors-community/el/7/x86_64/
-enabled=1
-gpgcheck=0
-
-[mysql-tools-community]
-name=MySQL Tools Community
-baseurl=http://repo.mysql.com/yum/mysql-tools-community/el/7/x86_64/
-enabled=1
-gpgcheck=0" >> /etc/yum.repos.d/mysql-community.repo
-
+gpgcheck=0" > /etc/yum.repos.d/mysql57-community.repo
 
 sudo dnf config-manager --disable mysql80-community
 sudo dnf config-manager --enable mysql57-community
-sudo dnf -y install mysql-community-server
 sudo rm -rvf /etc/mysql && sudo rm -rvf /var/lib/mysql
+sudo dnf -y install mysql-community-server
 systemctl start mysqld && sudo systemctl enable --now mysqld.service && systemctl status mysqld
 sudo grep 'A temporary password' /var/log/mysqld.log |tail -1
