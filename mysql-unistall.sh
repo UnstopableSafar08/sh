42e901363df1656e3e86afbc7337a4b76037d340
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-01-30 12:46:00 +0545

    Update mysql-unistall.sh
    v1.2

--- mysql-unistall.sh
+++ mysql-unistall.sh
@@ -3,7 +3,7 @@
 # Author : Sagar Malla
 # Date : 2024-Jan-21
 # Desc : This Script uninstall the MySQL
-# Version : v1.1
+# Version : v1.2
 ##########################################
 
 set -x
@@ -12,8 +12,9 @@ sudo yum remove mysql* -y
 sudo rm -rvf /etc/mysql && sudo rm -rvf /var/lib/mysql
 rpm -qa | grep mysql 
 rpm -qa mysql-*
-yum remove -y mysql-community-libs-5.7.43-1.el7.x86_64
-yum remove -y  mysql-community-common-5.7.43-1.el7.x86_64
+
+sudo yum remove -y mysql-community-*
+
 rpm -e --nodeps mysql57-community-release
 rpm -e --nodeps mysql80-community-release
 
