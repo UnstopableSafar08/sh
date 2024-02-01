d1f7744a8d0b41b6b0e54018506d2a948fe97b73
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-02-01 16:23:15 +0545

    Create rhel8-mysql57.sh
    v1.5

---
+++ rhel8-mysql57.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+##########################################
+# Author : Sagar Malla
+# Date : 2024-Jan-21
+# Desc : This Script install the MySQl5.7 on RHEL
+# Version : v1.6
+##########################################
+
+set -x
+subscription-manager repos --enable codeready-builder-for-rhel-8-$(arch)-rpms
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf remove @mysql -y
+sudo dnf -y module reset mysql
+sudo dnf -y module disable mysql
+sudo touch /etc/yum.repos.d/mysql-community.repo
+
+echo -e "[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/x86_64/
+enabled=1
+gpgcheck=0" > /etc/yum.repos.d/mysql57-community.repo
+
+sudo dnf config-manager --disable mysql80-community
+sudo dnf config-manager --enable mysql57-community
+sudo rm -rvf /etc/mysql && sudo rm -rvf /var/lib/mysql
+sudo dnf -y install mysql-community-server
+systemctl start mysqld && sudo systemctl enable --now mysqld.service && systemctl status mysqld
+sudo grep 'A temporary password' /var/log/mysqld.log |tail -1
