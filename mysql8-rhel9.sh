f85eb1edf3c03c805b9b6cab0d9c61d14efc1747
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2024-02-11 11:33:24 +0545

    Update mysql8-rhel9.sh
    v1.5

--- mysql8-rhel9.sh
+++ mysql8-rhel9.sh
@@ -3,7 +3,7 @@
 # Author : Sagar Malla
 # Date : 2024-Jan-20
 # Desc : This Script install the MySQl8 
-# Version : v1.4
+# Version : v1.5
 ##########################################
 echo "------------------------------------------------------------"
 echo "This Script file install the MySQL8 on your Machine."
@@ -11,7 +11,7 @@ echo "------------------------------------------------------------"
 set -x 
 subscription-manager repos --enable codeready-builder-for-rhel-9-$(arch)-rpms
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-dnf install -y wget curl
+#dnf install -y wget curl
 # --------------------- For Local .repo ------------------------------------
 # sudo touch /etc/yum.repos.d/mysql8_local.repo ## For Local repo
 # # insert into file
@@ -25,7 +25,7 @@ dnf install -y wget curl
 # sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql 
 # sudo yum install -y mysql-server
 # ---------------------Local .repo end ---------------------------------
-sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
 sudo wget https://repo.mysql.com//mysql80-community-release-el9-1.noarch.rpm
 sudo dnf localinstall mysql80-community-release-el9-1.noarch.rpm -y
 ###dnf repolist enabled | grep "mysql.*-community.*"
